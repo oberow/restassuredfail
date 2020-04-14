17ef6cf2c997844568bee7c4d004ffcbfbe0ac89
timothy goh <timothysl_goh@certisgroup.com>
2020-04-14 23:09:48 +0800

    add project

---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class test {
+    public static void main (String[] args) {
+        RestAssured.baseURI = "tim";
+    }
+}
